06afd24d2e838fb921825d309a96a1743d2c57d2
Sophia Da Silva <scd5533@g.rit.edu>
2026-08-04 00:07:09 -0400

    Added a functionality to coin processor and also fixed up the format_text.

--- main.py
+++ main.py
@@ -32,17 +32,19 @@ resources = {
 
 def format_text(cm_resources):
     """Formats the resources data into a printable format"""
+    global user_money
     cm_resources_water = cm_resources["water"]
     cm_resources_milk = cm_resources["milk"]
     cm_resources_coffee = cm_resources["coffee"]
-    temp_money = 0
     return (f"Water: {cm_resources_water}ml\n"
             f"Milk: {cm_resources_milk}ml\n"
             f"Coffee: {cm_resources_coffee}g\n"
-            f"Money: ${temp_money}")
+            f"Money: ${user_money}")
 
 # TODO: 4. process coins as inputs from the user and calculate the users value
-def coin_process(menu_choice):
+def coin_process(menu_choice, user_choice):
+    """Takes in the menus price of the users choice and sees if the users transaction was successful"""
+    global user_money
     print("Please insert coins.")
     quarter = int(input("How many quarters?: ")) * 0.25
     dime = int(input("How many dimes?: ")) * 0.10
@@ -50,15 +52,23 @@ def coin_process(menu_choice):
     pennies = int(input("How many pennies?: ")) * 0.01
 
     user_total = quarter + dime + nickle + pennies
-    rounded_total = round(user_total, 2)
-    print(f"You have ${rounded_total}")
+    # rounded_total = round(user_total, 2)
+    print(f"You have ${user_total}")
 
     # TODO: 5. checking if the transaction was successful
+    cost_of_coffee = menu_choice[user_choice]["cost"]
 
-
-
-
-
+    if cost_of_coffee > user_total:
+        print("Sorry that's not enough money. Money refunded.")
+    elif cost_of_coffee == user_total:
+        user_money += cost_of_coffee
+    else:
+        user_money += cost_of_coffee
+        rounded_total = round(user_total - cost_of_coffee, 2)
+        print(f"Here is ${rounded_total} dollars in change")
+    # need to fix it so that money goes into
+    print(f"money: {user_money}")
+    return user_money
 
 
 # TODO: 3. Check if resources are sufficient
@@ -77,27 +87,27 @@ def check_resources(users_choice, cm_resources, menu_choice):
                 still_resources = True
 
     return still_resources
-
     # this is for testing purposes to see if the resources are being allocated appropriately
     print(format_text(coffee_resources))
 
 
 
 still_on = True
+user_money = 0
 
 while still_on:
     # TODO: 1. Ask the user what coffee they would like
     users_drink = input("What would you like? (espresso/latte/cappuccino): ").lower()
 
     # TODO: 2. Print report of all coffee machine resources, and turn off the machine.
-    if users_drink == "off":
-        still_on = False
-    elif users_drink == "report":
+    if users_drink == "report":
         # need to update money 0 is being used as a temp
-        format_text(resources)
+        print(format_text(resources))
+    elif users_drink == "off":
+        still_on = False
     elif users_drink == "espresso" or users_drink == "latte" or users_drink == "cappuccino":
         # check_resources(users_drink, resources, MENU)
-        coin_process(MENU)
+        coin_process(MENU, users_drink)
     else:
         print("Please enter either 'off' or 'report'.")
 
