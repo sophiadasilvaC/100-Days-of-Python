fcf605369e421bff4b033f7204cbd0698789b7e6
Sophia Da Silva <scd5533@g.rit.edu>
2026-08-03 22:38:12 -0400

    added functionality to check_resources so now it will grab each key of the ingredients and see if it's there.

--- main.py
+++ main.py
@@ -36,30 +36,46 @@ def format_text(cm_resources):
     cm_resources_milk = cm_resources["milk"]
     cm_resources_coffee = cm_resources["coffee"]
     temp_money = 0
-    return (f"Water: {cm_resources_water}ml\n "
-            f"Milk: {cm_resources_milk}ml\n "
-            f"Coffee: {cm_resources_coffee}g"
+    return (f"Water: {cm_resources_water}ml\n"
+            f"Milk: {cm_resources_milk}ml\n"
+            f"Coffee: {cm_resources_coffee}g\n"
             f"Money: ${temp_money}")
 
 # TODO: 3. Check if resources are sufficient
-def check_resources():
-    return
+def check_resources(users_choice, coffee_resources, menu_choice):
+    """Takes in users input and checks if there are enough resources available for the order"""
+    for key in coffee_resources:
+        if key in menu_choice[users_choice]["ingredients"]:
+            if coffee_resources[key] < menu_choice[users_choice]["ingredients"][key]:
+                print(f"Sorry there is not enough {key}")
+                break
+            coffee_resources[key] -= menu_choice[users_choice]["ingredients"][key]
+
+    # this is for testing purposes to see if the resources are being allocated appropriately
+    print(format_text(coffee_resources))
 
 
-# TODO: 1. Ask the user what coffee they would like
-def coffee_machine():
-    still_on = True
-    while still_on:
-        users_drink_choice = input("What would you like? (espresso/latte/cappuccino):").lower()
-        # TODO: 2. Print report of all coffee machine resources.
-        if users_drink_choice == "off":
-            still_on = False
-        elif users_drink_choice == "report":
-            # need to update money 0 is being used as a temp
-            format_text(resources)
+still_on = True
+
+while still_on:
+    # TODO: 1. Ask the user what coffee they would like
+    users_drink = input("What would you like? (espresso/latte/cappuccino):").lower()
+
+    # TODO: 2. Print report of all coffee machine resources, and turn off the machine.
+    if users_drink == "off":
+        still_on = False
+    elif users_drink == "report":
+        # need to update money 0 is being used as a temp
+        format_text(resources)
+    elif users_drink == "espresso":
+        check_resources(users_drink, resources, MENU)
+    elif users_drink == "latte":
+        check_resources(users_drink, resources, MENU)
+    elif users_drink == "cappuccino":
+        check_resources(users_drink, resources, MENU)
+        print("Please enter either 'off' or 'report'.")
 
 
-coffee_machine()
 
 
 
