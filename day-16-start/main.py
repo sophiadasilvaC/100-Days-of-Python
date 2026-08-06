0fd8eb9a6fe5d8cfdc8edfa56eb1d0197c1bd20e
Sophia Da Silva <scd5533@g.rit.edu>
2026-08-06 15:36:31 -0400

    Messed around with turtle for a little bit.

--- day-16-start/main.py
+++ day-16-start/main.py
@@ -1,4 +1,17 @@
+# import another_module
+# print(another_module.another_variable)
 
-from another_module import another_variable
+from turtle import Turtle, Screen
 
-print(another_variable)
+# making a turtle object called timmy using the Turtle() class
+timmy = Turtle()
+print(timmy)
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
+
+
