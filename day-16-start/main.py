40517d01a17ab6c6faf600cfa55c723168fade64
Sophia Da Silva <scd5533@g.rit.edu>
2026-08-06 15:51:08 -0400

    Used prettytable to create objects by using the PrettyTable class.

--- day-16-start/main.py
+++ day-16-start/main.py
@@ -1,17 +1,31 @@
 # import another_module
 # print(another_module.another_variable)
 
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# # making a turtle object called timmy using the Turtle() class
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-# making a turtle object called timmy using the Turtle() class
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("coral")
-timmy.forward(100)
+from  prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+table.align = "l"
+
+print(table)
+
+
+
+
+
 
 
