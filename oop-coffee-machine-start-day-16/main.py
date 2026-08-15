eae0cbfc19936548f7cef0ce5691bee509a5bbef
Sophia Da Silva <scd5533@g.rit.edu>
2026-08-14 21:00:10 -0400

    went through the coffee machine in my day 15 and used oop to shorten the code from before.

--- oop-coffee-machine-start-day-16/main.py
+++ oop-coffee-machine-start-day-16/main.py
@@ -1,3 +1,24 @@
-from menu import Menu
+from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+
+menu = Menu()
+coffee_maker = CoffeeMaker()
+money_machine = MoneyMachine()
+
+still_on = True
+
+while still_on:
+    options = menu.get_items()
+    choice = input(f"What would you like? ({options}): ").lower()
+    if choice == "off":
+        still_on = False
+    elif choice == "report":
+        coffee_maker.report()
+        money_machine.report()
+    else:
+        drink = menu.find_drink(choice)
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+            coffee_maker.make_coffee(drink)
+
