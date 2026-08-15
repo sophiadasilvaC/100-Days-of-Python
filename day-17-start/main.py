26c8644cef10cc7b42aca13b4a01d70e33147ee5
Sophia Da Silva <scd5533@g.rit.edu>
2026-08-15 17:27:11 -0400

    Went through how to initialize an object and how to make a class.

---
+++ day-17-start/main.py
@@ -0,0 +1,11 @@
+# To declare a class you do "class Class_Name:"
+# if you don't want errors, and you plan on leaving your class empty use pass to continue to the next line of code
+class User:
+    pass
+
+# this is how to initialize an object from a class "object_name = Class_Name()"
+# needs to have parenthesis at the end in order to initialize the object to the class
+user_1 = User()
+def function():
+    pass
+print("Hello")
