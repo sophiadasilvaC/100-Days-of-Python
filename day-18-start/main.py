f6cc44f4721f328606296d1ecafd2f1b91295199
Sophia Da Silva <scd5533@g.rit.edu>
2026-08-18 19:44:32 -0400

    finished challenge 5 with python tuples and learned how to generate random RGB Colors.

--- day-18-start/main.py
+++ day-18-start/main.py
@@ -1,12 +1,21 @@
 # from turtle import * - this imports everything, but it's better to import the specified thing from the module
 # if its being used more than 3 times, if used once or twice import whole module
 # from turtle import Turtle
+import turtle
 import turtle as t
 import random
 
 tim = t.Turtle()
 
-colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+turtle.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    rand_color = (r, g, b)
+    return rand_color
+
 directions = [0, 90, 180, 270]
 tim.pensize(15)
 tim.speed("fastest")
@@ -14,7 +23,7 @@ tim.speed("fastest")
 for _ in range(200):
     tim.forward(30)
     tim.setheading(random.choice(directions))
-    tim.color(random.choice(colours))
+    tim.color(random_color())
     tim.pensize(15)
     tim.speed("fastest")
 
