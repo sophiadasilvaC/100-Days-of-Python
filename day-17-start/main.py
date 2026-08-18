f68c0362707107344721e82145447ee56a3564df
Sophia Da Silva <scd5533@g.rit.edu>
2026-08-17 23:12:00 -0400

    Working through constructors and the __init__ function.

--- day-17-start/main.py
+++ day-17-start/main.py
@@ -1,11 +1,28 @@
-# To declare a class you do "class Class_Name:"
-# if you don't want errors, and you plan on leaving your class empty use pass to continue to the next line of code
+# Just notes for me
+# To declare a class you do "class ClassName:"
+# if you don't want errors, and you plan on leaving your class empty use "pass"' to continue to the next line of code
 class User:
-    pass
+    def __init__(self, user_id, username):
+        self.id = user_id
+        self.username = username
+        self.followers = 0
 
-# this is how to initialize an object from a class "object_name = Class_Name()"
+# this is how to initialize an object from a class "object_name = ClassName()"
 # needs to have parenthesis at the end in order to initialize the object to the class
-user_1 = User()
-def function():
-    pass
-print("Hello")
+# Use PascalCase for the classes and in python and snake_case for everything else
+
+# made a new user with these staring values, much more convenient: lessens code
+user_1 = User("001", "sophia")
+user_2 = User("002", "Rachel")
+print(f"{user_1.id}, {user_1.username}, {user_2.id}, {user_2.username}, {user_1.followers}")
+
+# Adding an attribute: a variable associated to an object
+# in this case we are adding the attribute and to the object
+# user_1.id = "001"
+# user_1.username = "sophia"
+# print(user_1.username)
+
+
+# Constructor: part of the blueprint that allows us to specify what should happen when our object is being constructed (AKA initializing
+# To set (variables, counters, switches, ect.) to their values
+# to create a constructor we us "def __init__(self)" to initialize the attributes
