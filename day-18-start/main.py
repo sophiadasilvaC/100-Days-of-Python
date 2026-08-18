c7ae234816c22f24003277595afd9a1e95d677cb
Sophia Da Silva <scd5533@g.rit.edu>
2026-08-18 19:27:26 -0400

    finished the drawing different shapes challenge. Did this by using for loops and functions.

--- day-18-start/main.py
+++ day-18-start/main.py
@@ -2,15 +2,33 @@
 # if its being used more than 3 times, if used once or twice import whole module
 # from turtle import Turtle
 import turtle as t
+import random
 
 tim = t.Turtle()
 
+colors = ["red", "green", "blue", "yellow", "cyan", "magenta"]
 
-for _ in range(15):
-    tim.forward(10)
-    tim.penup()
-    tim.forward(10)
-    tim.pendown()
+def draw_shape(num_sides):
+    angle = 360 / num_sides
+    for _ in range(num_sides):
+        tim.forward(100)
+        tim.right(angle)
+
+for shape_side_n in range(3, 11):
+    tim.color(random.choice(colors))
+    draw_shape(shape_side_n)
+
+
+
+
+
+
+
+# for _ in range(15):
+#     tim.forward(10)
+#     tim.penup()
+#     tim.forward(10)
+#     tim.pendown()
 
 
 
