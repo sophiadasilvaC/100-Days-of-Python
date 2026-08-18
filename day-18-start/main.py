8fcc1cf506b269b2290a918c23f6f723e2988a36
Sophia Da Silva <scd5533@g.rit.edu>
2026-08-18 19:37:43 -0400

    finished challenge 4 doing a random walk.

--- day-18-start/main.py
+++ day-18-start/main.py
@@ -6,18 +6,39 @@ import random
 
 tim = t.Turtle()
 
-colors = ["red", "green", "blue", "yellow", "cyan", "magenta"]
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+directions = [0, 90, 180, 270]
+tim.pensize(15)
+tim.speed("fastest")
 
-def draw_shape(num_sides):
-    angle = 360 / num_sides
-    for _ in range(num_sides):
-        tim.forward(100)
-        tim.right(angle)
+for _ in range(200):
+    tim.forward(30)
+    tim.setheading(random.choice(directions))
+    tim.color(random.choice(colours))
+    tim.pensize(15)
+    tim.speed("fastest")
 
 
-for shape_side_n in range(3, 11):
-    tim.color(random.choice(colors))
-    draw_shape(shape_side_n)
+
+
+
+
+
+
+
+
+
+
+# def draw_shape(num_sides):
+#     angle = 360 / num_sides
+#     for _ in range(num_sides):
+#         tim.forward(100)
+#         tim.right(angle)
+
+#
+# for shape_side_n in range(3, 11):
+#     tim.color(random.choice(colors))
+#     draw_shape(shape_side_n)
 
 
 
