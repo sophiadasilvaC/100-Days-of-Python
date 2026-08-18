364a6246da334c5dc23d52770fcefb48038b9c2f
Sophia Da Silva <scd5533@g.rit.edu>
2026-08-18 19:28:12 -0400

    finished challenge 3, drawing different shapes challenge. Did this by using for loops and functions.

--- day-18-start/main.py
+++ day-18-start/main.py
@@ -14,6 +14,7 @@ def draw_shape(num_sides):
         tim.forward(100)
         tim.right(angle)
 
+
 for shape_side_n in range(3, 11):
     tim.color(random.choice(colors))
     draw_shape(shape_side_n)
