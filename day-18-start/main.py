56239727f35f952d80a7d494d277814420d86660
Sophia Da Silva <scd5533@g.rit.edu>
2026-08-19 13:56:10 -0400

    Finished up challenge 5, drawing a spirograph.

--- day-18-start/main.py
+++ day-18-start/main.py
@@ -14,21 +14,18 @@ def random_color():
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
-    rand_color = (r, g, b)
-    return rand_color
+    color = (r, g, b)
+    return color
 
-directions = [0, 90, 180, 270]
-tim.pensize(15)
+# directions = [0, 90, 180, 270]
 tim.speed("fastest")
+def draw_spirograph(size_of_gap):
+    for _ in range(int(360 / size_of_gap)):
+        tim.color(random_color())
+        tim.circle(100)
+        tim.setheading(tim.heading() + size_of_gap)
 
-for _ in range(200):
-    tim.forward(30)
-    tim.setheading(random.choice(directions))
-    tim.color(random_color())
-    tim.pensize(15)
-    tim.speed("fastest")
-
-
+draw_spirograph(5)
 
 
 
